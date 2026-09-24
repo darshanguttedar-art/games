2d5a8275c6202950d1df323f5310892823f751bc
Darshan <darshanguttedar98@gmail.com>
2026-09-24 19:25:09 +0530

    Add Mad Libs game

---
+++ Madlibsgame.py
@@ -0,0 +1,19 @@
+#Madlibs game 
+#word game where you create a story by filling in the blanks with words of your choice
+adjective1 = input("Enter an adjective: ")
+noun1 = input("Enter a noun (person, place, thing): ")
+adjective2 = input("Enter an adjective: ")
+verb1 = input("Enter a verb: ")
+adjective3 = input("Enter an adjective: ")
+place1 = input("Enter a place: ")
+noun2 = input("Enter another noun: ")
+verb2 = input("Enter another verb: ")
+feeling1 = input("Enter a feeling: ")
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"I saw a {noun1}.")
+print(f"{noun1} was {adjective2} and {verb1}.")
+print(f"I was {adjective3} to see a {noun1} {verb1} in the water.")
+print(f"After that, I walked to {place1} with a {noun2}.")
+print(f"The {noun2} started to {verb2} while everyone watched.")
+print(f"I felt {feeling1} and hurried home before sunset.")

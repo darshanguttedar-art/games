d27e37f4442158e5e751bca44270130e12bfbf24
Darshan <darshanguttedar98@gmail.com>
2026-09-25 22:05:12 +0530

    Add basic calculator program

---
+++ basiccalculator.py
@@ -0,0 +1,18 @@
+operator=input("enter an operator (+,-,*,/) :")
+
+num1=float(input("enter the first number:"))
+num2=float(input("enter the 2nd number:"))
+
+
+
+if operator=="+":
+    print(f"num1 + num2={num1+num2}")
+
+elif operator=="-":
+    print(f"substration of num1 - num2 :{num1-num2}")
+
+elif operator=="*":
+    print("mltiplication of the num1 *num2:{num*num2}")
+
+else:
+    print(f"division num1/num2:{num1/num2}")

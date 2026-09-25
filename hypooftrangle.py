b426d69d375172cf02b2a9e343f685d0d0a2d9ee
Darshan <darshanguttedar98@gmail.com>
2026-09-25 21:48:28 +0530

    Add hypotenuse triangle program

---
+++ hypooftrangle.py
@@ -0,0 +1,9 @@
+import math 
+
+a=float (input("enter the first side of a triangle :"))
+b=float (input("enter the second side of a triangle :"))    
+
+c=math.sqrt(pow(a,2)+pow(b,2))
+
+print(f"the hypotenuse of a triangle is {c} ")
+
